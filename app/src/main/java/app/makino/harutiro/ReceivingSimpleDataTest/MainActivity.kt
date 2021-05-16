cb08e4e6ea38ca2472145fcb08b8bf06bccb4c26
harutiro <harutiro2027@outlook.jp>
2021-05-16 13:23:58 +0900

    add: アイコンを読めるようにして、ドメインに分割してくれるクラスを作った。

--- app/src/main/java/app/makino/harutiro/ReceivingSimpleDataTest/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/ReceivingSimpleDataTest/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.PixelFormat
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowManager
@@ -13,8 +15,8 @@ import android.webkit.WebViewClient
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
+import androidx.core.graphics.createBitmap
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,52 +40,67 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.textView).text = text
 
+        val domein = UrlDomein().hen(text)
+
+
+        findViewById<TextView>(R.id.textView2).text = domein.toString()
+
+
+
+        Picasso.get()
+            //いらすとやの画像URL
+            .load("https://www.google.com/s2/favicons?domain=$domein")
+            .resize(300, 300) //表示サイズ指定
+            .centerCrop() //resizeで指定した範囲になるよう中央から切り出し
+            .into(findViewById<ImageView>(R.id.imageView)) //imageViewに流し込み
 
 
 
 
-//        WebViewでの画面の取得の仕方。
-
-        val webview = WebView(this)
-
-//        画面を取得するときにここがないと取得できない
-        val wm= getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val params = WindowManager.LayoutParams(
-            1,
-            1,
-            WindowManager.LayoutParams.TYPE_APPLICATION,
-            WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-            PixelFormat.TRANSLUCENT
-        )
-
-        wm.addView(webview, params)
 
 
-
-//      URL
-        webview.loadUrl( "https://blog.foresta.me/posts/getting_started_kotlin_and_coroutines/")
-
-//        画像取得部分
-        webview.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView, url: String) {
-
-
-
-                val picture = view.capturePicture()
-
-                var bitmap = Bitmap.createBitmap(view.width, view.width, Bitmap.Config.ARGB_8888)
-                val c = Canvas(bitmap)
-                picture.draw(c)
-
-//                BAG: なんか知らないけど、デバッグツールを使うと読めたり読めなかったりする。
-                var img: Bitmap? = view.favicon //?:Bitmap.createBitmap(1,1,Bitmap.Config.ARGB_8888)
-
-//                画像取得部分
-                findViewById<ImageView>(R.id.imageView).setImageBitmap(img)
-
-
-
-            }
-        }
+////        WebViewでの画面の取得の仕方。
+//
+//        val webview = WebView(this)
+//
+////        画面を取得するときにここがないと取得できない
+//        val wm= getSystemService(Context.WINDOW_SERVICE) as WindowManager
+//        val params = WindowManager.LayoutParams(
+//            1,
+//            1,
+//            WindowManager.LayoutParams.TYPE_APPLICATION,
+//            WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+//            PixelFormat.TRANSLUCENT
+//        )
+//
+//        wm.addView(webview, params)
+//
+//
+//
+////      URL
+//        webview.loadUrl( "https://blog.foresta.me/posts/getting_started_kotlin_and_coroutines/")
+//
+////        画像取得部分
+//        webview.webViewClient = object : WebViewClient() {
+//            override fun onPageFinished(view: WebView, url: String) {
+//
+//
+//
+//                val picture = view.capturePicture()
+//
+//                var bitmap = Bitmap.createBitmap(view.width, view.width, Bitmap.Config.ARGB_8888)
+//                val c = Canvas(bitmap)
+//                picture.draw(c)
+//
+////                BAG: なんか知らないけど、デバッグツールを使うと読めたり読めなかったりする。
+//                var img: Bitmap? = view.favicon //?:Bitmap.createBitmap(1,1,Bitmap.Config.ARGB_8888)
+//
+////                画像取得部分
+//                findViewById<ImageView>(R.id.imageView).setImageBitmap(img)
+//
+//
+//
+//            }
+//        }
     }
 }
